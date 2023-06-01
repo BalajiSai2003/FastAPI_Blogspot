a1461bc4ed8fb02f86f51b6313cea5a81b80d888
Balaji <104773951+BalajiSai2003@users.noreply.github.com>
2023-06-01 13:22:40 +0530

    env

--- Oauth2.py
+++ Oauth2.py
@@ -5,13 +5,16 @@ from fastapi import Depends,status,HTTPException
 from fastapi.security import OAuth2PasswordBearer 
 from dotenv import dotenv_values
 
-config = dotenv_values(".env")
+from config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-#secret key
-#algorithm
-#Expiration time
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
+# SECRET_KEY
+# Algorithm
+# Expriation time
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data : dict):
@@ -21,7 +24,7 @@ def create_access_token(data : dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode["exp"] = expire
-    encoded_jwt = jwt.encode(to_encode ,config.get("SECRET_KEY") ,algorithm=config.get("ALGORITHM"))
+    encoded_jwt = jwt.encode(to_encode ,SECRET_KEY ,algorithm=ALGORITHM)
 
     return encoded_jwt
 
@@ -30,7 +33,7 @@ def verify_access_token(token : str, credentisl_exception):
 
     try:
 
-        payload = jwt.decode(token, config.get("SECRET_KEY"),algorithms=[config.get("ALGORITHM")])
+        payload = jwt.decode(token, SECRET_KEY,algorithms=[ALGORITHM])
 
         id : str  = payload.get("user_id")
 
--- database.py
+++ database.py
@@ -2,17 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import dotenv_values
+from config import settings
 
-config = dotenv_values(".env")
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-engine = create_engine(config.get("SQLALCHEMY_DATABASE_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
